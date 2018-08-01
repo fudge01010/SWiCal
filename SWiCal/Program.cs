07c58211849ad6f7147071f7e03264bce75b32b6
Jack Fuge <jack.fuge@gmail.com>
2018-08-01 10:00:04 +1000

    added project skeleton.

---
+++ SWiCal/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ical.Net;
+using Ical.Net.DataTypes;
+using System;
+
+namespace SWiCal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            var now = DateTime.Now;
+            var later = now.AddDays(1);
+
+            //repeat for 3 days
+            var rrule = new RecurrencePattern(FrequencyType.Daily, 1) { Count = 3 };
+        }
+    }
+}
